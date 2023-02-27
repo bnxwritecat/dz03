f1fa8744f21ed63404a6411c44f5152a420d6b59
Chingiz Sultanov <118419172+bnxwritecat@users.noreply.github.com>
2023-02-27 18:41:19 +0300

    dz3

--- homeWorks/hw3.py
+++ homeWorks/hw3.py
@@ -24,3 +24,29 @@ print(ban.money_x())
 print(ban._kill())
 print(ban.user("bekbolot", 100))
 print(ban._Bank__jacpot())
+#########################################
+class Calculator:
+    def __init__(self, num):
+        self.num = num
+
+    def __add__(self, other):
+        print("Сложение")
+        return self.num + other.num
+
+    def __sub__(self, other):
+        print("Вычитание")
+        return self.num - other.num
+
+    def __mul__(self, other):
+        print("Умножение")
+        return self.num * other.num
+
+    def __truediv__(self, other):
+        print("Деление")
+        return self.num / other.num
+a = Calculator(1500)
+b = Calculator(1240)
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
